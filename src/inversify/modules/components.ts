6002f34f71df912353fc4a0fe3b2817b924cf26f
Thomas Hourlier <thomas.hourlier@cnode.fr>
2016-06-23 16:23:14 +0200

    chore(package): Update inversify

--- src/inversify/modules/components.ts
+++ src/inversify/modules/components.ts
@@ -1,4 +1,4 @@
-import { IKernel } from 'inversify';
+import { KernelModule, interfaces } from 'inversify';
 
 import { AbstractActionsCreator } from '../../actionsCreator';
 import { IAnnotator, Annotator } from '../../annotator';
@@ -14,11 +14,11 @@ import {
   LIFECYCLE_MANAGER_CONSTRUCTOR,
 } from '../identifiers';
 
-export default function componentsModule(kernel: IKernel): void {
-  kernel.bind<IAnnotator>(ANNOTATOR).to(Annotator);
-  kernel.bind<IEnhancer>(ENHANCER).to(Enhancer);
+export default new KernelModule((bind: interfaces.Bind) => {
+  bind<IAnnotator>(ANNOTATOR).to(Annotator);
+  bind<IEnhancer>(ENHANCER).to(Enhancer);
 
-  kernel.bind<typeof AbstractApi>(API_CONSTRUCTOR).toConstantValue(AbstractApi);
-  kernel.bind<typeof AbstractActionsCreator>(ACTIONS_CREATOR_CONSTRUCTOR).toConstantValue(AbstractActionsCreator);
-  kernel.bind<typeof AbstractLifecycleManager>(LIFECYCLE_MANAGER_CONSTRUCTOR).toConstantValue(AbstractLifecycleManager);
-}
+  bind<typeof AbstractApi>(API_CONSTRUCTOR).toConstantValue(AbstractApi);
+  bind<typeof AbstractActionsCreator>(ACTIONS_CREATOR_CONSTRUCTOR).toConstantValue(AbstractActionsCreator);
+  bind<typeof AbstractLifecycleManager>(LIFECYCLE_MANAGER_CONSTRUCTOR).toConstantValue(AbstractLifecycleManager);
+});
